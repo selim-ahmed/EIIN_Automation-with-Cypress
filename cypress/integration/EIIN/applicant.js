2148926b57c5fcce7b46d37419f39be174e71f8d
Selim <selimahmed1305@gmail.com>
2022-06-06 14:57:48 +0600

    updated

--- cypress/integration/EIIN/applicant.js
+++ cypress/integration/EIIN/applicant.js
@@ -22,21 +22,32 @@ const applicantuser = new applicantUser()
  //login
  it('Login EIIN', () => {
    
-    applicantuser.login('mitu',123)
+    applicantuser.login('mhossain','hossain')
 
   })
 
-   //Basic Information
-  it('Basic Information of EIIN Application', () => {
-
-    applicantuser.basicInfo('Jugipara School-4','জুগিপাড়া স্কুল-4')
+  //EIIN Application
+  it('EIIN Application', () => {
+    applicantuser.EIINapplication()
     
   })
+
+   
+  // //Basic Information
+  // it('Basic Information of EIIN Application', () => {
+
+  //   applicantuser.basicInfo('Jugipara School-4','জুগিপাড়া স্কুল-4')
+    
+  // })
    
      //Address
-  it('Address', () => {  
+  //it('Address', () => {  
 
-    applicantuser.address(8,60,492,7017,56942,496,
-      'The school was established in 1832 as Rangpur Zamindar School,by the local Zaminders or landlords.')
+    // applicantuser.address('4')
 
-  })  
+    // applicantuser.address(8,60,492,7017,56942,496,
+    //   'The school was established in 1832 as Rangpur Zamindar School,by the local Zaminders or landlords.')
+    //,'Dhaka','Mohammadpur','WARD NO-32 (45)','BABAR ROAD','Mohammadpur Housing - 1207',
+    //'The school was established in 1832 as Rangpur Zamindar School,by the local Zaminders or landlords.'
+
+  //})  
--- cypress/integration/EIIN/pageobjects.js
+++ cypress/integration/EIIN/pageobjects.js
@@ -8,9 +8,8 @@ export class applicantUser{
 
     }
        
-    
     applyForEIIN(){
-        cy.contains('APPLY FOR EIIN').click()
+        cy.contains('APPLY FOR EIIN/REVIEW').click()
         cy.wait(2000)
         
     }
@@ -19,70 +18,83 @@ export class applicantUser{
         cy.get(':nth-child(1) > .form-control').type(username)
         cy.get(':nth-child(2) > .form-control').type(pass)
         cy.get('.btn-md').click()
-          
+        cy.wait(3000) 
     }
 
-   
-    basicInfo(instituteNameEN, instituteNameBN ){
-        cy.get(':nth-child(2) > .form-control').type(instituteNameEN)
-        cy.get(':nth-child(3) > .form-control').type(instituteNameBN)
+    EIINapplication(){
+
+        cy.get(':nth-child(3) > .accordion-item > .d-flex > :nth-child(1) > .sidebar-text').click()
+        //cy.get(':nth-child(3) > .accordion-item > .accordion-collapse > .multi-level > .flex-column > :nth-child(1) > .nav-link > :nth-child(1) > .sidebar-text').trigger('mouseover')
+        cy.wait(3000)
+        cy.get(':nth-child(3) > .accordion-item > .accordion-collapse > .multi-level > .flex-column > :nth-child(1) > .nav-link > :nth-child(1) > .sidebar-text', {timeout: 5000}).contains('Apply For EIIN').click()
+        
+        //cy.get('div.dropdown', {timeout:10000}).contains('Admin').click()
+
+        //cy.get('div.subdropdown', {timeout:10000}).contains('Catalog Admin').should('be.visible')
+
+        //cy.get('div.subdropdown').contains('Catalog Admin').click({ force: true })
+     
     }
 
-    address(division, district, thana, union, mouza,postOffice, address){
-        cy.get(':nth-child(5) > :nth-child(1) > .form-control').select(division).should('have.value', division)
-        cy.get(':nth-child(6) > :nth-child(1) > .form-control').select(district).should('have.value', district)
-        cy.get(':nth-child(7) > :nth-child(1) > .form-control').select(thana).should('have.value' , thana)
-        cy.get(':nth-child(8) > :nth-child(1) > .form-control').select(union).should('have.value' , union)
-        cy.get(':nth-child(9) > :nth-child(1) > .form-control').select(mouza).should('have.value' , mouza)
-        cy.get(':nth-child(10) > :nth-child(1) > .form-control').select(postOffice).should('have.value', postOffice)
-        cy.get('.mb-3 > .form-control').type(address)
+    address(division){
+
+       cy.get(':nth-child(5) > :nth-child(1) > .form-control').select(division).should('have.value', division)
+        
+        
+        
+        // cy.get(':nth-child(6) > :nth-child(1) > .form-control').select(district).should('have.value', district)
+        // cy.get(':nth-child(7) > :nth-child(1) > .form-control').select(thana).should('have.value' , thana)
+        // cy.get(':nth-child(8) > :nth-child(1) > .form-control').select(union).should('have.value' , union)
+        // cy.get(':nth-child(9) > :nth-child(1) > .form-control').select(mouza).should('have.value' , mouza)
+        // cy.get(':nth-child(10) > :nth-child(1) > .form-control').select(postOffice).should('have.value', postOffice)
+        // cy.get('.mb-3 > .form-control').type(address)
         
     }
 
-    contactInfo(telephone, mobile, alternateMobile, fax, email, website){
-        cy.get(':nth-child(13) > .form-control').type(telephone)
-        cy.get(':nth-child(14) > .form-control').type(mobile)
-        cy.get(':nth-child(15) > .form-control').type(alternateMobile)
-        cy.get(':nth-child(16) > .form-control').type(fax)
-        cy.get(':nth-child(17) > .form-control').type(email)
-        cy.get(':nth-child(18) > .form-control').type(website)
+    // contactInfo(telephone, mobile, alternateMobile, fax, email, website){
+    //     cy.get(':nth-child(13) > .form-control').type(telephone)
+    //     cy.get(':nth-child(14) > .form-control').type(mobile)
+    //     cy.get(':nth-child(15) > .form-control').type(alternateMobile)
+    //     cy.get(':nth-child(16) > .form-control').type(fax)
+    //     cy.get(':nth-child(17) > .form-control').type(email)
+    //     cy.get(':nth-child(18) > .form-control').type(website)
         
-    }
+    // }
 
-    instituteInfo(
-        instituteType, 
-        InstituteForWhome, 
-        NoOfShift, 
-        constituency,
-        management, 
-        areaStatus, 
-        geoPosition, 
-        region,
-        dagNo, 
-        totalLand, 
-        latitude, 
-        longitude){
-        cy.get(':nth-child(20) > :nth-child(1) > .form-control').select(instituteType).should('have.value' ,instituteType)
-        cy.get(':nth-child(21) > :nth-child(1) > .form-control').select(InstituteForWhome).should('have.value' ,InstituteForWhome)
-        cy.get(':nth-child(22) > .form-control').type(NoOfShift)
-        cy.get('label.ml-5 > input').first().check()
-        cy.get(':nth-child(24) > :nth-child(1) > .form-control').select(constituency).should('have.value', constituency)
-        cy.get(':nth-child(25) > :nth-child(1) > .form-control').select(management).should('have.value', management)
-        cy.get(':nth-child(26) > :nth-child(1) > .form-control').select(areaStatus).should('have.value' , areaStatus)
-        cy.get(':nth-child(27) > :nth-child(1) > .form-control').select(geoPosition).should('have.value', geoPosition)
-        cy.get(':nth-child(28) > :nth-child(1) > .form-control').select(region).should('have.value' ,region)
-        cy.get(':nth-child(29) > .form-control').type(dagNo)
-        cy.get(':nth-child(30) > .form-control').type(totalLand)
-        cy.get(':nth-child(31) > .form-control').type(latitude)
-        cy.get(':nth-child(32) > .form-control').type(longitude)
+    // instituteInfo(
+    //     instituteType, 
+    //     InstituteForWhome, 
+    //     NoOfShift, 
+    //     constituency,
+    //     management, 
+    //     areaStatus, 
+    //     geoPosition, 
+    //     region,
+    //     dagNo, 
+    //     totalLand, 
+    //     latitude, 
+    //     longitude){
+    //     cy.get(':nth-child(20) > :nth-child(1) > .form-control').select(instituteType).should('have.value' ,instituteType)
+    //     cy.get(':nth-child(21) > :nth-child(1) > .form-control').select(InstituteForWhome).should('have.value' ,InstituteForWhome)
+    //     cy.get(':nth-child(22) > .form-control').type(NoOfShift)
+    //     cy.get('label.ml-5 > input').first().check()
+    //     cy.get(':nth-child(24) > :nth-child(1) > .form-control').select(constituency).should('have.value', constituency)
+    //     cy.get(':nth-child(25) > :nth-child(1) > .form-control').select(management).should('have.value', management)
+    //     cy.get(':nth-child(26) > :nth-child(1) > .form-control').select(areaStatus).should('have.value' , areaStatus)
+    //     cy.get(':nth-child(27) > :nth-child(1) > .form-control').select(geoPosition).should('have.value', geoPosition)
+    //     cy.get(':nth-child(28) > :nth-child(1) > .form-control').select(region).should('have.value' ,region)
+    //     cy.get(':nth-child(29) > .form-control').type(dagNo)
+    //     cy.get(':nth-child(30) > .form-control').type(totalLand)
+    //     cy.get(':nth-child(31) > .form-control').type(latitude)
+    //     cy.get(':nth-child(32) > .form-control').type(longitude)
         
-    }
+    // }
 
-    saveToNext(){
-        // cy.get('.mb-10 > .f-right').click()
-        cy.contains('Save & Next').click();
+    // saveToNext(){
+    //     // cy.get('.mb-10 > .f-right').click()
+    //     cy.contains('Save & Next').click();
         
-    }
+    // }
     
     
 }
